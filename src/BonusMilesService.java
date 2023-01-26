0d52e4bb83ef7c8c4225081bf2b02c4770248c06
ALEKSANDER <melsmaxms@gmail.com>
2023-01-26 18:50:57 +0700

    Fix2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        int bonus =0;
-        if (price >= 20) {
-            bonus = price / 20;
-        }
-        return bonus;
+    public int calculate(int cost) {
+        return cost / 20;
+
     }
 }
 
